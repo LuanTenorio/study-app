9cef248c72ee9e3e49706aad09c8a70e51737061
Luan <lluantenorio7@gmail.com>
2024-03-15 19:45:10 -0300

    Add estructure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EnemComponent } from './enem/enem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "enem", component: EnemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
